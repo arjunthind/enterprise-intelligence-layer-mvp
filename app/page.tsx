5efdfb0b358f69b0a75120b766d3f2e271c7fcaa
Arjun Thind <arjunthind@Arjuns-MacBook-Air.local>
2026-05-03 21:56:05 -0400

    Match agent preview action sizing

--- app/page.tsx
+++ app/page.tsx
@@ -392,7 +392,7 @@ function AgentsView({
                   Use in Demo
                 </button>
               ) : (
-                <button type="button" className="secondary compact-action" disabled>
+                <button type="button" className="preview-action" disabled>
                   Configured Preview
                 </button>
               )}
